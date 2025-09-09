3b7067fb90f429f7e623237c335ea09dbb850cbf
Geethasri08 <727722euai018@skcet.ac.in>
2025-09-09 15:39:33 +0530

    add content

--- test.js
+++ test.js
@@ -1,2 +1,7 @@
 console.log("This is a changes I have made");
 console.log("This is a test file in matri-morphosis");
+let session = "gitsession";
+let topic="gitbasics";
+let duration="2hrs";
+let user="geetha";
+console.log(session,topic,duration,user);
