1b94792a3320556c857d3bd36acae057a13e3c41
Geethasri08 <727722euai018@skcet.ac.in>
2025-09-09 15:44:27 +0530

    both changes

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 console.log("This is a changes I have made");
 console.log("This is a test file in matri-morphosis");
-let session = "gitsession";
-let topic="gitbasics";
+let session = "Git Basics";
+let topic="git session";
 let duration="2.5hrs";
 let user="geetha";
 console.log(session,topic,duration,user);
