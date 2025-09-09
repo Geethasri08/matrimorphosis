85abf6ce6cbf268eaaf3add113ed343017f21654
Geethasri08 <124983735+Geethasri08@users.noreply.github.com>
2025-09-09 15:43:32 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 console.log("This is a changes I have made");
 console.log("This is a test file in matri-morphosis");
-let session = "gitsession";
+let session = "gitsession2";
 let topic="gitbasics";
 let duration="2.5hrs";
 let user="geetha";
-console.log(session,topic,duration,user);
+console.log(session,topic,duration);
